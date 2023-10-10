b2c2d02a5fadfa6b0e0d35c70cb3645bdf72c59f
Marcelino Saquende <102360616+msaquende18@users.noreply.github.com>
2023-10-10 14:42:30 +0100

    admin dashboad actualizado

--- frontend-booklet-app/src/components/dashboard/AdSidebar.jsx
+++ frontend-booklet-app/src/components/dashboard/AdSidebar.jsx
@@ -1,46 +1,91 @@
 import React from 'react'
-import { Sidebar } from "flowbite-react";
+import { Badge, Sidebar } from "flowbite-react";
 import {
   HiArrowSmRight,
   HiChartPie,
   HiInbox,
+  HiOutlineCloudUpload,
   HiShoppingBag,
   HiTable,
   HiUser,
-  HiViewBoards,
+  HiViewBoards
 } from "react-icons/hi";
+import {
+  BiBuoy
+} from "react-icons/bi";
+
+import imagemUsuario from '../../assets/fotoPerfil.jpg';
+
 
 const AdSidebar = () => {
   return (
     <Sidebar aria-label="Sidebar with multi-level dropdown example">
+      <Sidebar.Logo href="#" img={imagemUsuario} imgAlt="Foto Admin">
+        <p>Booklet Admin Panel</p>
+      </Sidebar.Logo>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
           <Sidebar.Item href="#" icon={HiChartPie}>
             <p>Dashboard</p>
           </Sidebar.Item>
           <Sidebar.Collapse icon={HiShoppingBag} label="E-commerce">
-            <Sidebar.Item href="#">Products</Sidebar.Item>
-            <Sidebar.Item href="#">Sales</Sidebar.Item>
-            <Sidebar.Item href="#">Refunds</Sidebar.Item>
-            <Sidebar.Item href="#">Shipping</Sidebar.Item>
+            <Sidebar.Item href="#">Productos</Sidebar.Item>
+            <Sidebar.Item href="#">Vendas</Sidebar.Item>
+            <Sidebar.Item href="#">Reembolsos</Sidebar.Item>
+            <Sidebar.Item href="#">Entregas</Sidebar.Item>
           </Sidebar.Collapse>
           <Sidebar.Item href="#" icon={HiInbox}>
-            <p>Inbox</p>
+            <p>Gerenciar Livros</p>
           </Sidebar.Item>
           <Sidebar.Item href="#" icon={HiUser}>
-            <p>Users</p>
+            <p>Usuários</p>
           </Sidebar.Item>
           <Sidebar.Item href="#" icon={HiShoppingBag}>
-            <p>Products</p>
+            <p>Loja</p>
           </Sidebar.Item>
           <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            <p>Sign In</p>
+            <p>Login</p>
           </Sidebar.Item>
           <Sidebar.Item href="#" icon={HiTable}>
-            <p>Sign Up</p>
+            <p>Registar</p>
+          </Sidebar.Item>
+
+          <Sidebar.Item href="#" icon={HiOutlineCloudUpload}>
+            <p>upload</p>
+          </Sidebar.Item>
+
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            <p>Documentação</p>
+          </Sidebar.Item>
+
+          <Sidebar.Item href="#" icon={BiBuoy}>
+            <p>Ajuda</p>
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
+      <Sidebar.CTA>
+        <div className="mb-3 flex items-center">
+          <Badge color="warning">Loja de Livros</Badge>
+          <button
+            aria-label="Close"
+            className="-m-1.5 ml-auto inline-flex h-6 w-6 rounded-lg bg-gray-100 p-1 text-cyan-900 hover:bg-gray-200 focus:ring-2 focus:ring-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
+            type="button"
+          ></button>
+        </div>
+        <div className="mb-3 text-sm text-cyan-900 dark:text-gray-400">
+          <p>
+            Caro Escritor Seja bem-vindo ao sistema da Booklet! Aqui voçê poderá
+            fazer a gestão dos seus livros como adicionar novos livros para
+            venda, actualizar livro e outras funcionalidades do sistema.
+          </p>
+        </div>
+        <a
+          className="text-sm text-cyan-900 underline hover:text-cyan-800 dark:text-gray-400 dark:hover:text-gray-300"
+          href="#"
+        >
+          <p>Ver mais detalhes de uso de sistema</p>
+        </a>
+      </Sidebar.CTA>
     </Sidebar>
   );
 }
